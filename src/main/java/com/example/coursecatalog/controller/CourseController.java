3b9394ebc5c5e871bef19b01e7317ecafa740b2f
marcosDLCS <marcos.dlcs@gmail.com>
2017-08-07 22:14:24 +0200

    Add CORS support to getCourses endpoint

--- src/main/java/com/example/coursecatalog/controller/CourseController.java
+++ src/main/java/com/example/coursecatalog/controller/CourseController.java
@@ -8,11 +8,10 @@ import org.springframework.stereotype.Component;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
 
 @Component
 @Path("/courses")
-public class CourseController{
+public class CourseController {
 
     @Autowired
     private CourseService courseService;
@@ -20,14 +19,17 @@ public class CourseController{
     @GET
     @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Course> getCourses(@QueryParam("order") String order){
-        return courseService.getCourses(order);
+    public Response getCourses(@QueryParam("order") String order) {
+        return Response.ok().entity(courseService.getCourses(order))
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+                .allow("OPTIONS").build();
     }
 
     @GET
     @Path("/test")
     @Produces(MediaType.TEXT_PLAIN)
-    public String getTest(){
+    public String getTest() {
         return "This is a successful test! - [Course]";
     }
 
@@ -35,7 +37,7 @@ public class CourseController{
     @Path("/")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
-    public Response postCourse(final Course course){
+    public Response postCourse(final Course course) {
         courseService.postCourse(course);
         return Response.status(201).entity("Course saved").build();
     }
